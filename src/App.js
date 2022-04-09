eaba86b4db6ab9b99502ff2c3a194db280adaa4a
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2022-04-09 17:41:25 +0300

    Added React Router & MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import Product from "./components/product/Product";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<ProductList />} />
+        <Route path="products/:productId" element={<Product />} />
+        <Route path="about" element={<About />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
     </div>
   );
-}
+};
+
+const Home = () => {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/products/1">Product</Link>
+      </nav>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+};
 
 export default App;
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import LinearProgress from '@mui/material/LinearProgress';
 import ProductPrice from "./ProductPrice";
 
 const Product = (props) => {
   const [productDetails, setProductDetails] = useState();
-
+  let params = useParams();
+  
   useEffect(() => {
     console.log("useEffect");
-    setProductDetails({ title: "Title", price: 3456 });
-  }, []);
+    let id = params.productId;
+    console.log(id);
+    requestProductDetails(id);
+  }, [params.productId]); 
+
+  async function requestProductDetails(id) {
+    const res = await fetch(`http://localhost:8080/api/products/${id}`);
+    const json = await res.json();
+
+    console.log(json);
+
+    setProductDetails(json.productDetails);
+  }
 
   return productDetails === undefined ? (
-    <div>loading...</div>
+    <LinearProgress />
   ) : (
     <div>
       <h1>{productDetails.title}</h1>
