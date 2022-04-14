60596ed6582f9cc041fc4475dee724ab566eac33
Oleh Ilchyshyn <o.ilchyshyn01@gmail.com>
2022-04-14 21:26:56 +0300

    Price provider w/ MUI styles

--- src/components/product/PriceProvider.jsx
+++ src/components/product/PriceProvider.jsx
@@ -1,7 +1,23 @@
-const PriceProvider = (props) => {
-  const { provider } = props;
+import { Box, Link, Avatar, Typography } from "@mui/material";
+import React from "react";
 
-  return <div>Store: {provider.name}</div>;
+const PriceProvider = ({ provider }) => {
+  return (
+    <Box display="block" maxWidth={80} justifyContent="center">
+      <Link href={provider.url} underline="none" target="_blank" rel="noopener">
+        <Box display="table-cell">
+          <Avatar
+            alt={provider.name}
+            src={provider.logoUrl}
+            variant="rounded"
+          />
+        </Box>
+        <Typography variant="body2" color="black">
+          {provider.name}
+        </Typography>
+      </Link>
+    </Box>
+  );
 };
 
 export default PriceProvider;
